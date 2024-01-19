c41d131608b1b5d3f4c7d253f8ea8359536e0111
Chris Metcalf <cmetcalf2022@gmail.com>
2024-01-19 13:27:43 -0500

    Updated settings.py

--- Personal_Website/settings.py
+++ Personal_Website/settings.py
@@ -29,8 +29,8 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-&psk#na5l=p3q8
 # DEBUG = True
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['web-production-cbf4.up.railway.app', '127.0.0.1']
-CSRF_TRUSTED_ORIGINS = ['https://web-production-cbf4.up.railway.app']
+ALLOWED_HOSTS = []
+CSRF_TRUSTED_ORIGINS = []
 
 # Application definition
 
