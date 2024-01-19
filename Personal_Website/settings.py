a52ff026ba07b54693dd9d1aac0d98d9dd0c3d1f
Chris Metcalf <cmetcalf2022@gmail.com>
2024-01-19 17:00:46 -0500

    Added the Expense Tracker app to the project.

--- Personal_Website/settings.py
+++ Personal_Website/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "Portfolio.apps.PortfolioConfig"
+    "Portfolio.apps.PortfolioConfig",
+    "ExpenseTracker.apps.ExpensetrackerConfig"
 ]
 
 MIDDLEWARE = [
